3cda4f74f55e09a8f109edb5d6b848d69fd3e482
Wenpin CHOU <wpchou@outlook.com>
2017-04-04 03:30:02 +0800

    rime set up

--- setup.sh
+++ setup.sh
@@ -1,20 +1,19 @@
-pkgs=("zsh" "vim" "emacs" "tmux" "autossh" "git")
-for ((i=0; i<${#pkgs[*]}; i++))
-do
-	sudo apt install ${pkgs[i]} -y
-done
+#pkgs=("zsh" "vim" "emacs" "tmux" "autossh" "git")
+#for ((i=0; i<${#pkgs[*]}; i++))
+#do
+#	brew install ${pkgs[i]}
+#done
 
 #git clone git@gitlab.com:wpchou/etc.git ~/etc
 #cd ~/etc/
-#git checkout origin/ubuntu
-#git checkout -b ubuntu
+#git checkout origin/macos
+#git checkout -b macos
 #git submodule init
 #git submodule update
 
 # emacs
 #git clone https://github.com/syl20bnr/spacemacs.git ~/.emacs.d
 #ln -s ~/etc/spacemacs/ ~/.spacemacs.d
-#cd ~/.spacemacs.d
 
 # zsh
 #git clone https://github.com/robbyrussell/oh-my-zsh.git ~/dev/share/oh-my-zsh
