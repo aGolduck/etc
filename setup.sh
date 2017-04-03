4f510a102b974576a8bf5ac057f41f5e03de54dd
wenpin <wpchou@outlook.com>
2017-04-03 06:43:35 -0400

    ubuntu init

--- setup.sh
+++ setup.sh
@@ -1,20 +1,22 @@
-# edit /etc/hosts.allow
-# reverse ssh if necessary, do not use sshd, it's not ok yet
-# sudo service ssh --ful-restart
-# autossh -M 20000 -CfnNT -R remote_host_binding_port:localhost:localhost_sshd_port wenpin@remote_host -p remote_host_sshd_port
-pkgs=("vim" "emacs" "tmux" "autossh" "git")
-
+pkgs=("zsh" "vim" "emacs" "tmux" "autossh" "git")
 for ((i=0; i<${#pkgs[*]}; i++))
 do
-	sudo apt install ${pkgs[i]}
+	sudo apt install ${pkgs[i]} -y
 done
 
-#ln -s ~/etc/zshrc ~/.zshrc
-#enable zsh
-#if [ -t 1 ]; then
-#	exe zsh
-#fi
-#git clone git@gitlab.com/wpchou/etc.git ~/etc
-#git clone htts://github.com/robbyrussell/oh-my-zsh.git ~/dev/share/oh-my-zsh
+#git clone git@gitlab.com:wpchou/etc.git ~/etc
+#cd ~/etc/
+#git checkout origin/ubuntu
+#git checkout -b ubuntu
+#git submodule init
+#git submodule update
+
+# emacs
 #git clone https://github.com/syl20bnr/spacemacs.git ~/.emacs.d
-#ln -s ~/etc/spacemacs ~/.spacemacs.d
+#ln -s ~/etc/spacemacs/ ~/.spacemacs.d
+#cd ~/.spacemacs.d
+
+# zsh
+#git clone https://github.com/robbyrussell/oh-my-zsh.git ~/dev/share/oh-my-zsh
+#ln -s ~/etc/zshrc ~/.zshrc
+#chsh -s /usr/bin/zsh
