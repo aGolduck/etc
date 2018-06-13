56b7ed83873074b1c3ec8e39fa5964d4947abbbd
wenpin <wpchou@outlook.com>
2018-06-13 14:15:28 +0800

    ubuntu, pkgsrc env, nvm

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -24,11 +24,12 @@ plugins=(
     sudo
     systemd
     tmux
+    ubuntu
     web-search
     yarn
 )
 source $ZSH/oh-my-zsh.sh
-source $HOME/.zshenv
+#source $HOME/.zshenv
 
 function ppgrep() {
     if [[ $1 == "" ]]; then
@@ -61,7 +62,21 @@ if exists percol; then
     bindkey '^R' percol_select_history
 fi
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+export LC_CTYPE
+LC_CTYPE="en_US.UTF-8"
+export LC_COLLATE
+LC_COLLATE="C"
+export LC_TIME
+LC_TIME="C"
+export LC_NUMERIC
+LC_NUMERIC="C"
+export LC_MONETARY
+LC_MONETARY="C"
+export LC_MESSAGES
+LC_MESSAGES="en_US.UTF-8"
+export LC_ALL
+LC_ALL=""
 
+export NVM_DIR="$HOME/.nvm"
+[[ -z "$NVM_DIR" ]] && export NVM_DIR="$HOME/.nvm"
+[[ -f "$NVM_DIR/nvm.sh" ]] && source "$NVM_DIR/nvm.sh"
