eab4f0a1477f5ecc01e5cf42a624bfc00623ffe5
wenpin <wpchou@outlook.com>
2018-07-04 20:09:58 +0800

    alias rm

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -104,3 +104,5 @@ LC_ALL=""
 export NVM_DIR="$HOME/.nvm"
 [[ -z "$NVM_DIR" ]] && export NVM_DIR="$HOME/.nvm"
 [[ -f "$NVM_DIR/nvm.sh" ]] && source "$NVM_DIR/nvm.sh"
+
+alias 'rm'='echo NO rm for you'
