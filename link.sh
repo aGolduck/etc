82cfbdc19bf1a31bf49db2a9c7c940ee87248745
wenpin <wpchou@outlook.com>
2019-06-19 11:59:54 +0800

    add vnc-startup

--- link.sh
+++ link.sh
@@ -2,3 +2,4 @@ ln -sf etc/share/gitconfig ~/.gitconfig
 ln -sf etc/share/mongorc.js ~/.mongorc.js
 ln -sf etc/share/zshrc.sh ~/.zshrc
 ln -sf etc/share/gemrc ~/.gemrc
+ln -sf ../etc/share/vnc-xstartup.sh ~/.vnc/xstartup
