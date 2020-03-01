101a359317860023f9ab9d31c73294c57d701e10
wenpin <wpchou@outlook.com>
2020-03-01 16:18:18 +0800

    set up short name repo dir link

--- link.sh
+++ link.sh
@@ -5,4 +5,10 @@ ln -si etc/share/tmux.conf ~/.tmux.conf
 ln -si ../etc/share/vnc-xstartup.sh ~/.vnc/xstartup
 ln -si etc/share/zshrc.sh ~/.zshrc
 ln -si etc/share/pam_enviroment ~/.pam_enviroment
-ln -si ~/etc/share/desktops/Spacemacs.desktop ~/.local/share/applications/Spacemacs.desktop
+ln -si ../../../etc/share/desktops/Spacemacs.desktop ~/.local/share/applications/Spacemacs.desktop
+
+ln -si g ~/github.com
+ln -si g ~/github
+ln -si b ~/blob
+ln -si r ~/repo
+ln -si r ~/repository
