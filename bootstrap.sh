90bf43237b0ed3a4bd55159b7b40e8e0a95bc20b
wenpin <wpchou@outlook.com>
2020-03-18 11:28:14 +0800

    emacs profile

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,4 +8,7 @@ ssh-keygen -q -t rsa -b 4096 -N '' -f ~/.ssh/mo_rsa 2>/dev/null <<< y >/dev/null
 ssh-keygen -q -t rsa -b 4096 -N '' -f ~/.ssh/tc1_rsa 2>/dev/null <<< y >/dev/null
 ssh-keygen -q -t rsa -b 4096 -N '' -f ~/.ssh/xps_rsa 2>/dev/null <<< y >/dev/null
 
+git clone https://github.com/plexus/chemacs.git ~/g/plexus/chemacs
+. ~/g/plexus/chemacs/install.sh
+
 source ./link.sh
--- emacs-profiles.el
+++ emacs-profiles.el
@@ -1,4 +1,5 @@
-(("default" . ((user-emacs-directory . "~/.emacs.d")))
+(("default" . ((user-emacs-directory . "~/g/syl20bnr/spacemacs")))
+ ("vanilla" . ((user-emacs-directory . "~/.emacs.d")))
  ("spacemacs" . ((user-emacs-directory . "~/g/syl20bnr/spacemacs")
                  (env . (("SPACEMACSDIR" . "~/.spacemacs.d")))))
  ("doom" . ((user-emacs-directory . "~/g/hlissner/doom-emacs")
