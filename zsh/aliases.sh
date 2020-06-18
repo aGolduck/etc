cde7e64094728668c1b626e27cf7b0e18b353070
wenpin <wpchou@outlook.com>
2020-06-18 22:59:21 +0800

    reorder aliases

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -1,3 +1,14 @@
+alias 'top'='top -c'
+alias 'e'='emacsclient -nw'
+alias 'rm'='rm -i '
+alias 'ghc'='stack ghc -- '
+alias 'ghci'='stack ghci -- '
+alias 'runghc'='stack runghc -- '
+
+alias 'mgt'='mongo mongodb://myoffer:D41d8cd98f00B204e9800998ecf8427E@dds-wz9db587cdce66b433270.mongodb.rds.aliyuncs.com:3717/myoffer'
+alias 'mgo'='mongo mongodb://myoffer:My0ffer2015@dds-wz942bd82f2bc1242309.mongodb.rds.aliyuncs.com:3717/myoffer'
+alias 'mgu'='mongo mongodb://myoffer:D41d8cd98f00B204e9800998ecf8427E@dds-wz9db587cdce66b433270.mongodb.rds.aliyuncs.com:3717/myoffer-unittest'
+
 ## Powershell style command alias
 # alias 'link'='ln' # link is used
 alias 'advanced-top'='atop'
@@ -67,14 +78,3 @@ alias 'unix-name'='uname'
 alias 'virtual-memory-statistics'='vmstat'
 alias 'web-get'='wget'
 alias 'word-count'='wc'
-
-alias 'top'='top -c'
-alias 'e'='emacsclient -nw'
-alias 'rm'='rm -i '
-alias 'ghc'='stack ghc -- '
-alias 'ghci'='stack ghci -- '
-alias 'runghc'='stack runghc -- '
-
-alias 'mgt'='mongo mongodb://myoffer:D41d8cd98f00B204e9800998ecf8427E@dds-wz9db587cdce66b433270.mongodb.rds.aliyuncs.com:3717/myoffer'
-alias 'mgo'='mongo mongodb://myoffer:My0ffer2015@dds-wz942bd82f2bc1242309.mongodb.rds.aliyuncs.com:3717/myoffer'
-alias 'mgu'='mongo mongodb://myoffer:D41d8cd98f00B204e9800998ecf8427E@dds-wz9db587cdce66b433270.mongodb.rds.aliyuncs.com:3717/myoffer-unittest'
