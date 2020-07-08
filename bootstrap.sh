26d0d9470a90a83a8a888791127e3af3f3dadbf9
wenpin <wpchou@outlook.com>
2020-07-08 11:00:05 +0800

    bootstrap github rsa

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,8 +4,8 @@ echo "source ~/.local/etc/zsh/path.sh" >> ~/.zprofile
 
 mkdir -p ~/.ssh
 ssh-keygen -q -t rsa -b 4096 -N '' -f ~/.ssh/ali1_rsa 2>/dev/null <<< y >/dev/null
+ssh-keygen -q -t rsa -b 4096 -N '' -f ~/.ssh/github.com_rsa 2>/dev/null <<< y >/dev/null
 ssh-keygen -q -t rsa -b 4096 -N '' -f ~/.ssh/imac_rsa 2>/dev/null <<< y >/dev/null
-ssh-keygen -q -t rsa -b 4096 -N '' -f ~/.ssh/mmac_rsa 2>/dev/null <<< y >/dev/null
 ssh-keygen -q -t rsa -b 4096 -N '' -f ~/.ssh/mo_rsa 2>/dev/null <<< y >/dev/null
 ssh-keygen -q -t rsa -b 4096 -N '' -f ~/.ssh/tc1_rsa 2>/dev/null <<< y >/dev/null
 ssh-keygen -q -t rsa -b 4096 -N '' -f ~/.ssh/xps_rsa 2>/dev/null <<< y >/dev/null
