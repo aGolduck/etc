b06a5f30cd41b7605bbf0de8709e1743bf32878d
wenpin <wpchou@outlook.com>
2020-07-13 16:28:00 +0800

    remove ssh rsa generator
    multiple rsa is not safer than just one

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,14 +2,6 @@ git clone https://github.com/wpchou/etc.git ~/.local/etc
 echo "source ~/.local/etc/zsh/init.sh" >> ~/.zshrc
 echo "source ~/.local/etc/zsh/path.sh" >> ~/.zprofile
 
-mkdir -p ~/.ssh
-ssh-keygen -q -t rsa -b 4096 -N '' -f ~/.ssh/ali1_rsa 2>/dev/null <<< y >/dev/null
-ssh-keygen -q -t rsa -b 4096 -N '' -f ~/.ssh/github.com_rsa 2>/dev/null <<< y >/dev/null
-ssh-keygen -q -t rsa -b 4096 -N '' -f ~/.ssh/imac_rsa 2>/dev/null <<< y >/dev/null
-ssh-keygen -q -t rsa -b 4096 -N '' -f ~/.ssh/mo_rsa 2>/dev/null <<< y >/dev/null
-ssh-keygen -q -t rsa -b 4096 -N '' -f ~/.ssh/tc1_rsa 2>/dev/null <<< y >/dev/null
-ssh-keygen -q -t rsa -b 4096 -N '' -f ~/.ssh/xps_rsa 2>/dev/null <<< y >/dev/null
-
 git clone https://github.com/plexus/chemacs.git ~/g/plexus/chemacs
 . ~/g/plexus/chemacs/install.sh
 
