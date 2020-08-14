5aa56e8f14ebeb6e3b0778c88879ac746bb4c31b
wenpin <wpchou@outlook.com>
2020-08-15 07:05:42 +0800

    clean bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,9 +2,6 @@ git clone https://github.com/wpchou/etc.git ~/.local/etc
 echo "source ~/.local/etc/zsh/init.sh" >> ~/.zshrc
 echo "source ~/.local/etc/zsh/path.sh" >> ~/.zprofile
 
-git clone https://github.com/plexus/chemacs.git ~/g/plexus/chemacs
-. ~/g/plexus/chemacs/install.sh
-
 tic -x -o ~/.terminfo terminfo-24bit.src
 
 source ./link.sh
