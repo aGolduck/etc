51288f084e9d11cde2a69bd0c56b108881c44b92
wenpin <wpchou@outlook.com>
2020-08-15 20:15:32 +0800

    tweak

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,5 @@
 git clone https://github.com/wpchou/etc.git ~/.local/etc
+git clone https://github.com/zsh-users/antigen.git ~/g/zsh/antigen
 echo "source ~/.local/etc/zsh/init.sh" >> ~/.zshrc
 echo "source ~/.local/etc/zsh/path.sh" >> ~/.zprofile
 
--- link.sh
+++ link.sh
@@ -1,13 +1,14 @@
 ETC_REPO='.local/etc'
 
+mkdir -p ~/.vnc
 ln -si ${ETC_REPO}/gemrc ~/.gemrc
 ln -si ${ETC_REPO}/gitconfig ~/.gitconfig
 ln -si ${ETC_REPO}/mongorc.js ~/.mongorc.js
 ln -si ${ETC_REPO}/tmux.conf ~/.tmux.conf
 ln -si ../${ETC_REPO}/vnc-xstartup.sh ~/.vnc/xstartup
-ln -si ${ETC_REPO}/zshrc.sh ~/.zshrc
 ln -si ${ETC_REPO}/pam_enviroment ~/.pam_enviroment
 
+mkdir -p ~/.local/share/applications
 # ln -si ${ETC_REPO}/emacs-profiles.el ~/.emacs-profiles.el
 # ln -si ../../../${ETC_REPO}/desktops/spacemacs.desktop ~/.local/share/applications/spacemacs.desktop
 # ln -si ../../../${ETC_REPO}/desktops/doom-emacs.desktop ~/.local/share/applications/doom-emacs.desktop
@@ -16,9 +17,9 @@ ln -si ../../../${ETC_REPO}/desktops/org-protocol.desktop ~/.local/share/applica
 ln -si ~/Applications/zotero/zotero.desktop ~/.local/share/applications/zotero.desktop
 
 mkdir -p ~/g ~/b ~/r
-ln -si g ~/com.github
+ln -si g ~/github.com
 ln -si g ~/github
 ln -si b ~/blob
 ln -si r ~/repo
 ln -si r ~/repository
-ln -si ../g ~/r/com.github
+ln -si ../g ~/r/github.com
