1fceea840923064f956f51cfbdd168508baff946
wenpin <wpchou@outlook.com>
2020-09-07 21:41:04 +0800

    fix path for kde gui applications

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,9 @@
 git clone https://github.com/wpchou/etc.git ~/.local/etc
 git clone https://github.com/zsh-users/antigen.git ~/g/zsh-users/antigen
 echo "source ~/.local/etc/zsh/init.sh" >> ~/.zshrc
+echo "source ~/.local/etc/zsh/variables.sh" >> ~/.zprofile
 echo "source ~/.local/etc/zsh/path.sh" >> ~/.zprofile
+echo "source ~/.zprofile" >> ~/.config/plasma-workspace/env/path.sh
 
 tic -x -o ~/.terminfo terminfo-24bit.src
 
