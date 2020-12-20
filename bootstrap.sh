4067633e354f5a0667f75ce55a84788833a790ab
wenpin <wpchou@outlook.com>
2020-12-20 17:40:08 +0800

    fix bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,8 +3,9 @@ git clone https://github.com/zsh-users/antigen.git ~/g/zsh-users/antigen
 echo "source ~/.local/etc/zsh/init.sh" >> ~/.zshrc
 echo "source ~/.local/etc/zsh/variables.sh" >> ~/.zprofile
 echo "source ~/.local/etc/zsh/path.sh" >> ~/.zprofile
+mkdir -p ~/.config/plasma-workspace/env
 echo "source ~/.zprofile" >> ~/.config/plasma-workspace/env/path.sh
 
-tic -x -o ~/.terminfo terminfo-24bit.src
+tic -x -o ~/.terminfo ~/.local/etc/terminfo-24bit.src
 
-source ./link.sh
+source ~/.local/etc/link.sh
