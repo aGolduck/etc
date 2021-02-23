9a07680044ecef805c854d1564286af75cee3c27
wenpin <wpchou@outlook.com>
2021-02-23 20:21:48 +0800

    remove ghc related aliases

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -1,9 +1,10 @@
 alias 'top'='top -c'
 alias 'e'='emacsclient -nw'
 alias 'rm'='rm -i '
-alias 'ghc'='stack ghc -- '
-alias 'ghci'='stack ghci -- '
-alias 'runghc'='stack runghc -- '
+## use ghcup, aliases below are no longer needed
+# alias 'ghc'='stack ghc -- '
+# alias 'ghci'='stack ghci -- '
+# alias 'runghc'='stack runghc -- '
 
 alias 'mgt'='mongo mongodb://myoffer:D41d8cd98f00B204e9800998ecf8427E@dds-wz9db587cdce66b433270.mongodb.rds.aliyuncs.com:3717/myoffer'
 alias 'mgo'='mongo mongodb://myoffer:My0ffer2015@dds-wz942bd82f2bc1242309.mongodb.rds.aliyuncs.com:3717/myoffer'
