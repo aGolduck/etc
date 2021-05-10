3d4ad4bce1d2ae7f55acf2eed740eb6d6fa2ec55
wenpin <wpchou@outlook.com>
2021-05-10 14:05:08 +0800

    remove mongo aliases

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -6,10 +6,6 @@ alias 'rm'='rm -i '
 # alias 'ghci'='stack ghci -- '
 # alias 'runghc'='stack runghc -- '
 
-alias 'mgt'='mongo mongodb://myoffer:D41d8cd98f00B204e9800998ecf8427E@dds-wz9db587cdce66b433270.mongodb.rds.aliyuncs.com:3717/myoffer'
-alias 'mgo'='mongo mongodb://myoffer:My0ffer2015@dds-wz942bd82f2bc1242309.mongodb.rds.aliyuncs.com:3717/myoffer'
-alias 'mgu'='mongo mongodb://myoffer:D41d8cd98f00B204e9800998ecf8427E@dds-wz9db587cdce66b433270.mongodb.rds.aliyuncs.com:3717/myoffer-unittest'
-
 ## Powershell style command alias
 # alias 'link'='ln' # link is used
 alias 'advanced-top'='atop'
