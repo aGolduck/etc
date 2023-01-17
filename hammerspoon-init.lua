807a08853bd3973b008b8d85277433e607156cd1
wenpin <wpchou@outlook.com>
2023-01-18 07:45:14 +0800

    disable hammerspoon emacs keybinding

--- hammerspoon-init.lua
+++ hammerspoon-init.lua
@@ -11,17 +11,16 @@ end
 
 -- hs.hotkey.bind({"alt"}, "M", open_app("/Users/w/.nix-profile/Applications/Emacs.app"))
 -- hs.hotkey.bind({"alt"}, "M", open_app("/usr/local/Cellar/emacs-mac/emacs-27.2-mac-8.2/Emacs.app"))
+-- hs.hotkey.bind({"alt"}, "M", open_app("Emacs"))
 hs.hotkey.bind({"alt"}, "M", function()
       current_app = hs.application.frontmostApplication()
       current_app_name= current_app:title()
-      hs.alert.show(current_app_name)
       if current_app_name == "IntelliJ IDEA" then
          hs.eventtap.keyStroke({"alt"}, "M", 0, current_app)
       else
          open_app("Emacs")()
       end
 end)
--- hs.hotkey.bind({"alt"}, "M", open_app("Emacs"))
 hs.hotkey.bind({"cmd"}, "right", function() hs.window.focusedWindow():moveOneScreenEast(true, true) end)
 hs.hotkey.bind({"cmd"}, "left", function() hs.window.focusedWindow():moveOneScreenWest(true, true) end)
 
