776835b07367959f93080e2d19eeef32ad6f245b
wpchou <wpchou@outlook.com>
2024-04-28 10:32:17 +0800

    hammperspoon 增加 intellij idea 远程与本地的快捷键
    M-c, M-l emacs 用于修改大小写，常用于英文写作，目前不常用
    M-c for client intellij idea
    M-l for local intellij idea

--- hammerspoon-init.lua
+++ hammerspoon-init.lua
@@ -1,3 +1,4 @@
+-- 此方法是使用 hammerspoon 自己的方法去激活,麻烦,用名字判断也失准,不如直接调用命令行 open
 function open_app(name)
    return function()
       -- hs.application.launchOrFocus(name)
@@ -15,16 +16,34 @@ end
 hs.hotkey.bind({"alt"}, "M", function()
       current_app = hs.application.frontmostApplication()
       current_app_name= current_app:title()
-      if current_app_name == "IntelliJ IDEA" then
+      current_app_bundle_id = current_app:bundleID()
+      -- if current_app_name == "IntelliJ IDEA" then
+      if current_app_bundle_id == "com.jetbrains.intellij" then
          hs.eventtap.keyStroke({"alt"}, "M", 0, current_app)
       else
-         open_app("Emacs")()
+         -- open_app("Emacs")()
+         hs.execute("open /opt/homebrew/Cellar/emacs-plus@29/29.3/Emacs.app")
       end
+end)
+hs.hotkey.bind({"alt"}, "L", function()
+      current_app = hs.application.frontmostApplication()
+      current_app_name= current_app:title()
+      if current_app_name == "Emacs" then
+         hs.eventtap.keyStroke({"alt"}, "L", 0, current_app)
+      else
+         hs.execute("open '/Users/w/Applications/IntelliJ IDEA Ultimate.app'")
+      end
+end)
+hs.hotkey.bind({"alt"}, "C", function()
+      current_app = hs.application.frontmostApplication()
+      current_app_name= current_app:title()
+      hs.execute("open '/Users/w/Library/Caches/JetBrains/JetBrainsClientDist/JetBrainsClient-233.15026.9-aarch64.sit-03ce3ecd7e.ide.d/IntelliJ IDEA.app/'")
 end)
 hs.hotkey.bind({"cmd", "shift"}, "right", function() hs.window.focusedWindow():moveOneScreenEast(true, true) end)
 hs.hotkey.bind({"cmd", "shift"}, "left", function() hs.window.focusedWindow():moveOneScreenWest(true, true) end)
 
-hs.hotkey.bind({"cmd", "ctrl"}, "L", function() hs.alert.show(hs.application.frontmostApplication():title()) end)
+-- hs.hotkey.bind({"cmd", "ctrl"}, "L", function() hs.alert.show(hs.application.frontmostApplication():title()) end)
+hs.hotkey.bind({"cmd", "ctrl"}, "L", function() hs.alert.show(hs.application.frontmostApplication():bundleID()) end)
 
 -- hs.hotkey.bind({"alt"}, "space", function() hs.eventtap.keyStroke({}, "escape"); hs.eventtap.keyStroke({}, "space") end)
 
